dadf99d93650796df62445b1fd7cb02c81a5dc47
Ringil <kylerob89@gmail.com>
2012-06-04 16:38:08 -0300

    Just displays a given image

--- ComicViewer.py
+++ ComicViewer.py
@@ -4,8 +4,6 @@ import Image            #PIL
 import ImageTk          #PIL
 import sys
 import getopt
-import zipfile
-import base64
 
 try:
     if len(sys.argv) == 1:
@@ -16,25 +14,12 @@ except Exception, e:
     sys.exit(1)
     
 root = Tk()
-    
-z = zipfile.ZipFile(sys.argv[1], "r")
+file = sys.argv[1]
 
-for file in z.namelist():
-    print file
-    bytes = z.read(file)
-    z.extract(file)
-
-    '''Extract the files. Make the folder hidden (by putting a . infront of it).
-    Concat the folder name with the next name in namelist and send it to Image.open'''
-    img = Image.open(file) #problem here
+img = Image.open(file) #problem here
     
-    canvas = Canvas(root, height=img.size[1]+20, width=img.size[0]+20)
-    canvas.pack(side=LEFT,fill=BOTH,expand=1)
-    photo = ImageTk.PhotoImage("Batman and Robin 09 (2012) (Minutemen-DosHalos)/"+img)
-    item = canvas.create_image(10,10,anchor=NW, image=photo)
-    mainloop()
-
-    
-
-    
-    
+canvas = Canvas(root, height=img.size[1]+20, width=img.size[0]+20)
+canvas.pack(side=LEFT,fill=BOTH,expand=1)
+photo = ImageTk.PhotoImage(img)
+item = canvas.create_image(10,10,anchor=NW, image=photo)
+mainloop()
