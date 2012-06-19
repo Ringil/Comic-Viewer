49bb129667d7b49482a0477903c6f0bee738186d
Mark Vensawhl <kylerob89@gmail.com>
2012-06-19 19:38:59 -0300

    refactored some stuff
    Problem getting the right encoding for loadFromData

--- ComicViewer.py
+++ ComicViewer.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python
 
 '''
+Written By: Kyle Robinson
 Rewritten in Qt4 instead of Tkinter
 Missing functionality:
 
@@ -8,51 +9,58 @@ Scrollbar
 Ability to read through multiple images
 Relative positioning of widgets instead of exact
 '''
-import sys, zipfile, 
+import sys, zipfile
 from PyQt4 import QtGui, QtCore
 from StringIO import StringIO
 
 class ComicViewer(QtGui.QWidget):
-    
-    def __init__(self):
+    def __init__(self, inFile):
         super(ComicViewer, self).__init__()
-        
         self.initUI()
+        self.openFile(inFile)
+        self.showImage(self.encodeImg())
         
     def initUI(self):      
+        self.hbox = QtGui.QHBoxLayout(self)
+        self.lbl = QtGui.QLabel(self)
 
-        hbox = QtGui.QHBoxLayout(self)
-        pixmap = QtGui.QPixmap("axe.gif") #this will have to change
-
-        lbl = QtGui.QLabel(self)
-        lbl.setPixmap(pixmap)
-
-        hbox.addWidget(lbl)
-        self.setLayout(hbox)
+        self.hbox.addWidget(self.lbl)
+        self.setLayout(self.hbox)
         
         self.move(300, 200)
         self.setWindowTitle('Comic Viewer')
         self.show() 
         
-    def readZip(self, inFile):
-    		'''
-    		This actually read and encodes the first file in a zipfile
-    		NEEDS TO CHANGE
-    		'''
-    		z = zipfile.ZipFile(inFile,"r")
-    		data = z.read(z.namelist()[0])
-    		self.dataEnc = StringIO(data)       
+    def showImage(self, imgData):
+        self.pixmap = QtGui.QPixmap()
+        self.pixmap.loadFromData(imgData)
+         
+        self.lbl.setPixmap(self.pixmap)
+        
+        self.show()
+    
+    def openFile(self, inFile):
+        self.z = zipfile.ZipFile(inFile, "r")
+        
+    def encodeImg(self):
+        '''
+    	This actually encodes ONLY the first file in a zipfile
+		NEEDS TO CHANGE
+		'''
+        #FIXME: NOT ENCODED THE WAY THAT LOADFROMDATA WANTS IT
+        data = self.z.read(self.z.namelist()[0])
+        return StringIO(data)       
         
 if __name__ == '__main__':
-	try:
-    		if len(sys.argv) == 1:
-        		raise ValueError, "No image filename specified"
-	except Exception, e:
-    		print >>sys.stderr, e
-    		print "USAGE: ./ComicViewer.py <comic filename>"
-    		sys.exit(1)
-    		
-    	inFile = sys.argv[1]	
-	app = QtGui.QApplication(sys.argv)
-	ex = ComicViewer()
-	sys.exit(app.exec_())
+    try:
+        if len(sys.argv) == 1:
+            raise ValueError, "No image filename specified"
+    except Exception, e:
+        print >> sys.stderr, e
+        print "USAGE: ./ComicViewer.py <comic filename>"
+        sys.exit(1)
+            
+    inFile = sys.argv[1]	
+    app = QtGui.QApplication(sys.argv)
+    ex = ComicViewer(inFile)
+    sys.exit(app.exec_())
