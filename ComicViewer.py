4e9d1942300c73f51bf56b55a06b34e9b69641d3
Mark Vensawhl <kylerob89@gmail.com>
2012-06-30 19:30:10 -0300

    check zip/rar

--- ComicViewer.py
+++ ComicViewer.py
@@ -2,14 +2,11 @@
 
 '''
 Written By: Kyle Robinson
-Rewritten in Qt4 instead of Tkinter
-Missing functionality:
-
-Ability to read through multiple images
-Relative positioning of widgets instead of exact
+Comic Viewer
 '''
 
 import sys, zipfile
+import rarfile
 from PIL import Image
 from PyQt4 import QtGui, QtCore
 from StringIO import StringIO
@@ -36,9 +33,9 @@ class ComicViewer(QtGui.QWidget):
         hbox.addWidget(scrollArea)
         
         self.setLayout(hbox)
-        self.setGeometry(1280, 1280, 1024, 1024)
+        self.setGeometry(800, 800, 600, 600)
         self.move(300, 200)
-        self.setWindowTitle('Batman')
+        self.setWindowTitle('Comic Viewer')
         self.show() 
         
     def showImage(self, imgData):
@@ -46,13 +43,18 @@ class ComicViewer(QtGui.QWidget):
         self.show()
     
     def openFile(self, inFile):
-        self.z = zipfile.ZipFile(inFile, "r")
+        if zipfile.is_zipfile(inFile) == True:      #Check if its a zip file (.zip, .cbz)
+            self.z = zipfile.ZipFile(inFile, "r")    
+        elif rarfile.is_rarfile(inFile) == True:    #Check if its a rar file (.rar, .cbr)
+            self.z = rarfile.RarFile(inFile, "r")
+        else:
+            print "Unknown Comic Archive Type"
+            sys.exit(1)
         
     def encodeImg(self):
         '''
         Returns a QPixmap of the first image in an archive
         '''
-        
         data = self.z.read(self.z.namelist()[0])
         enc = StringIO(data) 
         img = Image.open(enc)
