5d119ab820cdadace0348871e580efa9ef9e1a72
Kyle Robinson <kylerob89@gmail.com>
2013-07-31 12:18:23 -0400

    Open file dialog added plus minor problems introduced
    Can now open files from within the program. Unfortunately images
    stretch to the entire window size instead of staying their normal
    height and width. This is an improvment over the 20x30px space I was
    getting before setting self.scrollArea.setWidgetResizable(True).
    
    Rar's mysteriously don't work again saying unrar is not installed again

--- ComicViewer.py
+++ ComicViewer.py
@@ -13,35 +13,44 @@ version so everything is GUI based.
 import sys, zipfile, rarfile
 import Utility
 from PySide import QtGui, QtCore
-from StringIO import StringIO
 
-class ComicViewer(QtGui.QWidget):
-    def __init__(self, inFile):
+class ComicViewer(QtGui.QMainWindow):
+    def __init__(self):
         super(ComicViewer, self).__init__()
-        self.openFile(inFile)
+        #self.openFile(inFile)
         self.initUI()
         #self.createDB()
         #self.insertDB("test.cbz", self.currentPage)
-        self.showImage(self.createPixmap(self.currentPage))
+        #self.showImage(self.createPixmap(self.currentPage))
     
     def initUI(self): 
-        hbox = QtGui.QHBoxLayout(self)
         pixmap = QtGui.QPixmap()
 
+        #Create a label to show the pixmap (comic)
         self.lbl = QtGui.QLabel(self)
         self.lbl.setPixmap(pixmap)
+        self.lbl.setSizePolicy(QtGui.QSizePolicy.Ignored, QtGui.QSizePolicy.Ignored)
+        self.lbl.setScaledContents(True)
+
+        #Create open file menu for selecting comics to open
+        openFile = QtGui.QAction(QtGui.QIcon('open.png'), 'Open', self)
+        openFile.setShortcut('Ctrl+O')
+        openFile.triggered.connect(self.openFile)
+
+        #Create the menubar
+        menubar = self.menuBar()
+        fileMenu = menubar.addMenu('&File')
+        fileMenu.addAction(openFile)
         
+        #Make the label clickable to go forward pages
         Utility.clickable(self.lbl).connect(self.changePage)
 
         #Create a scrollbar for the label
-        scrollArea = QtGui.QScrollArea(self)
-        scrollArea.setBackgroundRole(QtGui.QPalette.Dark)
-        scrollArea.setWidget(self.lbl)
-        scrollArea.setWidgetResizable(True)
-        
-        hbox.addWidget(scrollArea)
-        
-        self.setLayout(hbox)
+        self.scrollArea = QtGui.QScrollArea()
+        self.scrollArea.setBackgroundRole(QtGui.QPalette.Dark)
+        self.scrollArea.setWidget(self.lbl)
+        self.scrollArea.setWidgetResizable(True)
+        self.setCentralWidget(self.scrollArea)
 
         #Find the resolution of the screen
         height = QtGui.QDesktopWidget().availableGeometry().height()
@@ -91,18 +100,24 @@ class ComicViewer(QtGui.QWidget):
         self.lbl.setPixmap(pixmap) 
         self.show()
     
-    def openFile(self, inFile):
+    def openFile(self):
         '''
         Open a file stream to either a rar/cbr or zip/cbz file
         '''
+        inFile, _ = QtGui.QFileDialog.getOpenFileName(self, 'Open file',
+                    '/home')
+
         self.currentPage = 0
         if zipfile.is_zipfile(inFile) == True:      #Check if its a zip file (.zip, .cbz)
             self.z = zipfile.ZipFile(inFile, "r")    
         elif rarfile.is_rarfile(inFile) == True:    #Check if its a rar file (.rar, .cbr)
-             self.z = rarfile.RarFile(inFile)
+            self.z = rarfile.RarFile(inFile)
         else:
             print("Unknown Comic Archive Type")
             sys.exit(1)
+
+        self.showImage(self.createPixmap(self.currentPage)) #Probably change how this works later
+
 
     def getNumPages(self):
         '''
@@ -123,15 +138,15 @@ class ComicViewer(QtGui.QWidget):
         return pix
         
 if __name__ == '__main__':
-    try:
-        if len(sys.argv) == 1:
-            raise ValueError("No image filename specified")
-    except Exception(e):
-        print >> sys.stderr, e
-        print("USAGE: ./ComicViewer.py <path to your comic file>")
-        sys.exit(1)
+    # try:
+    #     if len(sys.argv) == 1:
+    #         raise ValueError("No image filename specified")
+    # except Exception(e):
+    #     print >> sys.stderr, e
+    #     print("USAGE: ./ComicViewer.py <path to your comic file>")
+    #     sys.exit(1)
             
-    inFile = sys.argv[1]	
+    # inFile = sys.argv[1]	
     app = QtGui.QApplication(sys.argv)
-    ex = ComicViewer(inFile)
+    ex = ComicViewer()
     sys.exit(app.exec_())
